fa7f87b540c369b52ce572e2a3ea5bc0d9dad9c0
Gentil le noir <gentillenoir075@gmail.com>
2025-12-13 00:18:07 +0200

    Correcting Docher configurations - 58 th commit,  almost done

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+# Créer .env si inexistant
+if [ ! -f .env ]; then
+    touch .env
+fi
+
 # Générer APP_KEY si vide
 if [ -z "$APP_KEY" ]; then
     php artisan key:generate --force
